fd3817ac8933751cf1069a6706f185800a67bc94
Sowmitra Guha <sowmmochy@gmail.com>
2025-07-21 00:08:44 +0600

    check updating the work

--- middlewires/firebaseAuth.js
+++ middlewires/firebaseAuth.js
@@ -2,20 +2,20 @@ const admin = require("firebase-admin");
 
 // ✅ Middleware to verify Firebase Token
 const verifyFirebaseToken = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
+    const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) return res.status(401).json({ message: "unauthorized access." });
+    if (!authHeader || !authHeader.startsWith('Bearer ')) return res.status(401).json({ message: "unauthorized access." });
 
-  const token = authHeader.split(" ")[1];
+    const token = authHeader.split(" ")[1];
 
-  try {
-    const decodedUser = await admin.auth().verifyIdToken(token);
-    req.user = decodedUser; // attach decoded Firebase user info
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: "Invalid or expired token", error });
-  }
-  //next();
+    try {
+        const decodedUser = await admin.auth().verifyIdToken(token);
+        req.user = decodedUser; // attach decoded Firebase user info
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: "Invalid or expired token", error });
+    }
+    //next();
 };
 
 module.exports = verifyFirebaseToken; 
