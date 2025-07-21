bdf8e6f6335881bde84a7a380a29748361d3a300
Sowmitra Guha <sowmmochy@gmail.com>
2025-07-22 00:46:23 +0600

    netlify link added

--- index.js
+++ index.js
@@ -16,17 +16,17 @@ const port = process.env.PORT || 5000;
 
 //cors setup
 app.use(cors());
-// const allowedOrigins = [
-//   "https://elaborate-khapse-da6469.netlify.app/", // ✅ Production frontend
-//   "http://localhost:5173"  // ✅ Local frontend
-// ];
+const allowedOrigins = [
+  "https://mellow-toffee-c45c62.netlify.app", // ✅ Production frontend
+  "http://localhost:5173"  // ✅ Local frontend
+];
 
-// app.use(
-//   cors({
-//     origin: allowedOrigins,
-//     credentials: true, // ✅ allow cookies/auth headers
-//   })
-// );
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true, // ✅ allow cookies/auth headers
+  })
+);
 
 
 
